53abd0df0e506acdcdaba2d78fe5e1f94f9e3835
Fadhil Luqman <fadhil.luqman@gmail.com>
2020-05-05 21:56:40 +0800

    Show inventory last updated date on login

--- lib/myppe_web/views/admin/admin_session_view.ex
+++ lib/myppe_web/views/admin/admin_session_view.ex
@@ -1,6 +1,6 @@
-defmodule MyppeWeb.AdminSessionView do
+defmodule MyppeWeb.Admin.AdminSessionView do
   use MyppeWeb, :view
-  alias MyppeWeb.AdminSessionView
+  alias MyppeWeb.Admin.AdminSessionView
 
   # def render("index.json", %{admin_sessions: admin_sessions}) do
   #   %{data: render_many(admin_sessions, AdminSessionView, "admin_session.json")}
@@ -33,7 +33,8 @@ defmodule MyppeWeb.AdminSessionView do
         social_media_website: admin.pharmacy.social_media_website,
         social_media_whatsapp: admin.pharmacy.social_media_other,
         state: admin.pharmacy.state,
-        opening_hours: Enum.map(admin.pharmacy.opening_hours, &get_opening_hour_structs/1)
+        opening_hours: Enum.map(admin.pharmacy.opening_hours, &get_opening_hour_structs/1),
+        updated_at: get_last_inventory_update(admin.pharmacy)
       }
     }
   end
@@ -43,5 +44,21 @@ defmodule MyppeWeb.AdminSessionView do
     |> Map.delete(:__meta__)
     |> Map.delete(:pharmacy)
     |> Map.from_struct
+  end
+
+  def get_last_inventory_update(pharmacy) do
+    pharmacy =
+      Myppe.Inventories.get_pharmacy!(pharmacy.id)
+      |> Myppe.Repo.preload(:stock_updates)
+    case pharmacy.stock_updates do
+      [] ->
+        nil
+      stock_updates ->
+        stock_updates
+        |> Enum.sort(fn x,y -> Timex.after?(x.inserted_at, y.inserted_at) end)
+        |> List.first()
+        |> Map.get(:inserted_at)
+    end
+
   end
 end
